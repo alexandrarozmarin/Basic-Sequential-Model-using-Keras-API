8ed08c57b5a9c87655975cbaec71a15286b0ae06
Alexandra Rozmarin <amr10191@nyu.edu>
2024-08-05 13:11:44 -0700

    adding validation set to check for overfitting no overfitting observed

--- SequentialModel.py
+++ SequentialModel.py
@@ -52,4 +52,4 @@ model.summary()
 
 #training model
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
