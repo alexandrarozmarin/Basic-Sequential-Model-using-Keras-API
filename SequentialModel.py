3e652090981419bd21d4238a6ae0851324ca72e8
Alexandra Rozmarin <amr10191@nyu.edu>
2024-08-05 13:48:56 -0700

    added predictions and confusion matrix

--- SequentialModel.py
+++ SequentialModel.py
@@ -52,4 +52,85 @@ model.summary()
 
 #training model
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=0)
+
+#creating a test set
+test_labels = []
+test_samples = []
+
+for i in range(10):
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(1) #did experience side effect
+
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+for i in range(200):
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(0) #did not experience side effect
+
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+
+#turning data into correct format
+test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
+test_labels, test_samples = shuffle(test_labels, test_samples)
+
+#scaling data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1, 1))
+
+#predict on test data
+predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=0)
+
+rounded_preductions = np.argmax(predictions, axis=-1)
+
+#create a confusion matrix
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_preductions)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+
+plt.show()
